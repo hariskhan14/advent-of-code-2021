12e853a0cca6e2c30d1fe8bb8e18cb616ae56da3
hariskhan14 <mohd_hariskhan14@hotmail.com>
2021-12-14 00:24:31 +0500

    refactoring

--- day2/day2.go
+++ day2/day2.go
@@ -6,18 +6,13 @@ import (
 )
 
 func LetsDive(commands []string) int {
-	result := 0
+	result := 1
 
 	for _, val := range commands {
 		split := strings.Split(val, " ")
 		commandValue, err := strconv.Atoi(split[1])
 		if err != nil {
 			return -1
-		}
-
-		if result == 0 {
-			result = commandValue
-			continue
 		}
 
 		result *= commandValue
