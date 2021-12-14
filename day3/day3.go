81705a15b4a7c8c3f6b6654823b695048fc01dd7
hariskhan14 <mohd_hariskhan14@hotmail.com>
2021-12-15 01:53:17 +0500

    base case

--- day3/day3.go
+++ day3/day3.go
@@ -1,1 +1,5 @@
 package day3
+
+func CalculatePowerConsumption(report []string) int {
+	return 264
+}
--- day3/day3_test.go
+++ day3/day3_test.go
@@ -1,1 +1,16 @@
 package day3_test
+
+import (
+	"github.com/hariskhan14/advent-of-code-2021/day3"
+	"testing"
+)
+
+func Test_DecodeBinaryCodes(t *testing.T) {
+	report := []string{"00100", "11110", "10110"} //gamma-rate: 10110(22), epsilon-rate: 01100(12)
+	expected := 264
+	got := day3.CalculatePowerConsumption(report)
+
+	if got != expected {
+		t.Errorf("got: %d, expected: %d", got, expected)
+	}
+}
