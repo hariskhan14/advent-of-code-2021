621b1c39e16d8a6690d08a12a314b8317212599f
hariskhan14 <mohd_hariskhan14@hotmail.com>
2021-12-15 03:05:32 +0500

    gofmt

--- day3/day3_test.go
+++ day3/day3_test.go
@@ -6,12 +6,12 @@ import (
 )
 
 func Test_DecodeBinaryCodes(t *testing.T) {
-	testSuites := []struct{
-		name string
-		report []string
+	testSuites := []struct {
+		name     string
+		report   []string
 		expected int
-	} {
-		{name: "base case", report: []string{"00100", "11110", "10111"}, expected: 198}, //gamma-rate: 10110(22), epsilon-rate: 01001(9)
+	}{
+		{name: "base case", report: []string{"00100", "11110", "10111"}, expected: 198},                    //gamma-rate: 10110(22), epsilon-rate: 01001(9)
 		{name: "adding another code", report: []string{"00100", "11110", "01110", "01011"}, expected: 238}, //gamma-rate: 01110(14), epsilon-rate: 10000(17)
 		{name: "sample input", report: []string{"00100", "11110", "10110", "10111", "10101", "01111", "00111", "11100", "10000", "11001", "00010", "01010"}, expected: 198},
 	}
